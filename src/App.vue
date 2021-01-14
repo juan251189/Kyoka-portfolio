eb94ef34558c201762bd10c905c828e308b7f11e
juan251189 <juan251189@hotmail.com>
2021-01-15 01:19:29 +1100

    specialities section compleated

--- src/App.vue
+++ src/App.vue
@@ -25,45 +25,46 @@
 
 
 
-    <section id="front">
-      <div class="row">
+  <section id="front">
+    <div class="row">
 
-        <div class="col-12" style="margin:0px;padding:0px;border:2px solid green;">
-          <img src="~@/assets/front-flowers.png" alt="" style="position:relative;min-width:100%;height:100vh;border-bottom:20px solid black;">
-          <div class="title-container">
-            <p class="title">KYOKA YAMASHITA</p>
-            <p class="subTitle">Japanese Supermodel & Entrepreneur</p>
+      <div class="col-12" style="margin:0px;padding:0px;border:2px solid green;">
+        <img src="~@/assets/front2.jpg" alt="" style="position:relative;min-width:100%;height:100vh;border-bottom:20px solid black;">
+        <div class="title-container">
+          <h1 class="title">KYOKA YAMASHITA</h1>
+          <p class="subTitle">Japanese Supermodel & Entrepreneur</p>
 
-          </div>
         </div>
-
       </div>
-    </section>
+
+    </div>
+  </section>
 
 
 
-      <section id="welcome-tour" class="container-fluid">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
-          <h3>
-          WELCOME TO OUR
+  <section id="welcome-tour" class="container-fluid">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h3 class="big-subheader">
+            WELCOME TO OUR
           </h3>
-          <h1>
+          <h1 class="big-header" style="color:black;">
             MODELING AGENCY...
           </h1>
         </div>
-          <div class="col-md-6">
-            <h4>FRINGILLA LACUS ORCI ET NUNC. SED DIGNISSIM, LECTUS NON MATTIS FAUCIBUS, NISL NIBH IACULIS ANTE, SIT AMET FRINGILLA VELIT EST SED DIAM. VESTIBULUM VESTIBULUM.</h4>
-            <p>Praesent accumsan ac velit vitae auctor. Proin pharetra consectetur leo, nec porta magna varius nec. Sed vehicula eros neque, a venenatis est lobortis id. Aenean id lectus sit amet odio pellentesque gravida. Suspendisse potenti. Praesent non nunc ac orci facilisis auctor. Cras metus quam, condimentum ut venenatis in, rutrum eget diam. Sed vehicula at risus quis fringilla</p>
-          </div>
+        <div class="col-md-6">
+          <h4>FRINGILLA LACUS ORCI ET NUNC. SED DIGNISSIM, LECTUS NON MATTIS FAUCIBUS, NISL NIBH IACULIS ANTE, SIT AMET FRINGILLA VELIT EST SED DIAM. VESTIBULUM VESTIBULUM.</h4>
+          <p>Praesent accumsan ac velit vitae auctor. Proin pharetra consectetur leo, nec porta magna varius nec. Sed vehicula eros neque, a venenatis est lobortis id. Aenean id lectus sit amet odio pellentesque gravida. Suspendisse potenti. Praesent
+            non nunc ac orci facilisis auctor. Cras metus quam, condimentum ut venenatis in, rutrum eget diam. Sed vehicula at risus quis fringilla</p>
         </div>
       </div>
+    </div>
 
-      </section>
+  </section>
 
 
-  <div id="news" class="container">
+  <div id="news" class="container-fluid">
     <div class="row">
       <div class="col-12"></div>
     </div>
@@ -72,7 +73,7 @@
   <div id="projects-campagin" class="container">
     <div class="row">
       <div class="col-md-4 projects">
-        <img src="~@/assets/front-flowers-nobackground.png" class="img-projects" alt="" style="position:absolute;max-width:400px;height:100%;right:0.1%;">
+        <img src="~@/assets/products1.jpg" class="img-projects" alt="" style="position:absolute;width:100%;height:100%;right:0.1%;">
 
         <h1 class="title-projects">Products</h1>
       </div>
@@ -82,7 +83,7 @@
         <h1 class="title-projects">Gallery</h1>
       </div>
       <div class="col-md-4 projects">
-        <img src="~@/assets/front-flowers-nobackground.png" class="img-projects" alt="" style="position:absolute;max-width:400px;height:100%;right:0.1%;">
+        <img src="~@/assets/fitness3.jpg" class="img-projects" alt="" style="position:absolute;width:100%;height:100%;right:0.1%;">
 
         <h1 class="title-projects">Work-out Plans</h1>
       </div>
@@ -94,25 +95,193 @@
 
 
 
-  <section id="catalog">
-    <div class="container">
 
-      <h2 class="catalog-header">NEW MODELS</h2>
+  <section id="professional-advice">
+    <div class="container-fluid">
       <div class="row">
-        <div class="col-md-3 col-sm-6"><div class="image-section"><img src="~@/assets/front-flowers.png" alt="" class="img-circle"><h4 class="catalog-text-header">Name</h4></div><p class="catalog-text">
 
-SFATI FRINCO
-Portugal
-pharetra consectetur lnec port magn. Sed vehicula eros nequelectus.</p></div>
-        <div class="col-md-3 col-sm-6"><img src="~@/assets/front-flowers.png" alt="" class="img-circle"><h4>Name</h4><p>sub name</p></div>
-        <div class="col-md-3 col-sm-6"><img src="~@/assets/front-flowers.png" alt="" class="img-circle"><h4>Name</h4><p>sub name</p></div>
-          <div class="col-md-3 col-sm-6"><img src="~@/assets/front-flowers.png" alt="" class="img-circle"><h4>Name</h4><p>sub name</p></div>
+        <div class="col-sm-6"><img src="" alt="" class="img-circle"></div>
+        <div class="col-sm-6 profesional-ad-content">
+          <h3>PROFESSIONAL ADVICE</h3>
+          <h2> THE BEST MODEL AGENCY IN THE WORLD</h2>
+
+          <p class="professional-ad-text">Cras metus quam, condimentum ut venen rutrum ediam. Sed vehicula at r quis fringnll lobortis fermentum dignissim fringilla lacus orci et nunc. Sed dignissim, lectus nmattis faucibus, nisl nibh iaculis ante,
+            sit amet fringilla velit sed diam. Vestibulum vestibulum ut dui quis iaculis.</p>
+          <button class="btn btn-danger">APPLY NOW</button>
+        </div>
       </div>
     </div>
   </section>
 
 
-https://www.gracethemes.com/demo/modeling/
+  <section id="specialities">
+    <div class="container-fluid">
+      <div class="specialities-headers">
+        <h2>SPECIALITIES OF OUR AGENCY
+
+        </h2>
+        <p>Praesent accumsan ac velit vitae auctor. Proin pharetra consectetur leo, nec porta magna varius nec. Sed vehicula eros neque, a venenatis est lobortis id. Aenean id lectus sit amet odio pellentesque gravida. Suspendisse potenti.
+
+        </p>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+
+
+
+
+
+    <div>
+
+        <div class="border"><span class='fas fa-heart' style='font-size:36px;float:left;background:red;padding:20px;border-radius:50%;color:white;margin-right:5px;'></span></div>
+
+      <div class="border">
+        <h3>EASY TO CONTACT
+        </h3>
+        <p style="">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusa doloremque laudantore veritatis.
+      </p></div>
+
+
+    </div>
+
+
+
+
+
+        </div>
+        <div class="col-md-4">
+          <div>
+
+              <div class="border"><span class='fas fa-heart' style='font-size:36px;float:left;background:red;padding:20px;border-radius:50%;color:white;margin-right:5px;'></span></div>
+
+            <div class="border">
+              <h3>EASY TO CONTACT
+              </h3>
+              <p style="">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusa doloremque laudantore veritatis.
+            </p></div>
+
+
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div>
+
+              <div class="border"><span class='fas fa-heart' style='font-size:36px;float:left;background:red;padding:20px;border-radius:50%;color:white;margin-right:5px;'></span></div>
+
+            <div class="border">
+              <h3>EASY TO CONTACT
+              </h3>
+              <p style="">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusa doloremque laudantore veritatis.
+            </p></div>
+
+
+          </div>
+        </div>
+
+        <div class="row mt-2">
+          <div class="col-md-4">
+            <div>
+
+                <div class="border"><span class='fas fa-heart' style='font-size:36px;float:left;background:red;padding:20px;border-radius:50%;color:white;margin-right:5px;'></span></div>
+
+              <div class="border">
+                <h3>EASY TO CONTACT
+                </h3>
+                <p style="">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusa doloremque laudantore veritatis.
+              </p></div>
+
+
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div>
+
+                <div class="border"><span class='fas fa-heart' style='font-size:36px;float:left;background:red;padding:20px;border-radius:50%;color:white;margin-right:5px;'></span></div>
+
+              <div class="border">
+                <h3>EASY TO CONTACT
+                </h3>
+                <p style="">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusa doloremque laudantore veritatis.
+              </p></div>
+
+
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div>
+
+                <div class="border"><span class='fas fa-heart' style='font-size:36px;float:left;background:red;padding:20px;border-radius:50%;color:white;margin-right:5px;'></span></div>
+
+              <div class="border">
+                <h3>EASY TO CONTACT
+                </h3>
+                <p style="">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusa doloremque laudantore veritatis.
+              </p></div>
+
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+    <section id="catalog">
+      <div class="container">
+
+        <h2 class="catalog-header">NEW MODELS</h2>
+        <div class="row">
+          <div class="col-md-3 col-sm-6">
+            <div class="image-section"><img src="~@/assets/front-flowers.png" alt="" class="img-circle">
+              <h4 class="catalog-text-header">Name</h4>
+            </div>
+            <p class="catalog-text">
+
+              SFATI FRINCO
+              Portugal
+              pharetra consectetur lnec port magn. Sed vehicula eros nequelectus.</p>
+          </div>
+          <div class="col-md-3 col-sm-6">
+            <div class="image-section"><img src="~@/assets/front-flowers.png" alt="" class="img-circle">
+              <h4 class="catalog-text-header">Name</h4>
+            </div>
+            <p class="catalog-text">
+
+              SFATI FRINCO
+              Portugal
+              pharetra consectetur lnec port magn. Sed vehicula eros nequelectus.</p>
+          </div>
+          <div class="col-md-3 col-sm-6">
+            <div class="image-section"><img src="~@/assets/front-flowers.png" alt="" class="img-circle">
+              <h4 class="catalog-text-header">Name</h4>
+            </div>
+            <p class="catalog-text">
+
+              SFATI FRINCO
+              Portugal
+              pharetra consectetur lnec port magn. Sed vehicula eros nequelectus.</p>
+          </div>
+          <div class="col-md-3 col-sm-6">
+            <div class="image-section"><img src="~@/assets/front-flowers.png" alt="" class="img-circle">
+              <h4 class="catalog-text-header">Name</h4>
+            </div>
+            <p class="catalog-text">
+
+              SFATI FRINCO
+              Portugal
+              pharetra consectetur lnec port magn. Sed vehicula eros nequelectus.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+  https://www.gracethemes.com/demo/modeling/
 
 
 </div>
@@ -129,21 +298,56 @@ export default {
 
 <style scoped>
 #app {
-  font-family: "monserrate", Arial, sans-serif;
+
   overflow-x: hidden;
 }
-.container{
+
+#app h1,
+h2,
+h3 {
+  font-family: 'Roboto', sans-serif;
+
+}
+
+#app p {
+  font-family: 'Assistant', sans-serif;
+
+
+}
+
+.container {
 
   padding: 0px;
 
 }
-.border{
 
+section {
+  margin-bottom: 70px;
 }
-.row{
+
+
+.row {
 
   padding: 0px;
-  margin:0px;
+  margin: 0px;
+}
+
+.big-header {
+  font-size: 55px;
+  line-height: 55px;
+  font-weight: 600;
+  margin: 0 0 40px;
+  color: #fff;
+  text-transform: uppercase;
+}
+
+.big-subheader {
+  font-size: 25px;
+  font-style: italic;
+  font-weight: 700;
+  margin: 0 0 40px;
+  color: #FE3333;
+  text-transform: uppercase;
 }
 
 
@@ -227,7 +431,7 @@ export default {
   padding: 0px;
   margin: 0px;
 
-  font-family: Teko, sans-serif;
+  font-family: Teko, sans-serif !important;
   font-size: 5em;
   color: white;
   font-weight: 500;
@@ -255,71 +459,71 @@ export default {
 
 #biography {
 
-  padding:70px 10px;
+  padding: 70px 10px;
 
 
-  font-family: sans-serif, monospace;
   font-size: 1.7em;
   font-weight: 700;
 
 }
 
 
-#projects-campagin {
+#projects-campagin {}
+
+.img-projects:hover {
+  opacity: 0.5;
 
 }
 
-.img-projects:hover{
-opacity: 0.5;
-
-}
-
-.projects{
+.projects {
 
   border: 1px solid green;
   width: 100%;
   height: 450px;
 
-/*  background-image: url('~@/assets/front-flowers-nobackground.png'); /* fallback */
-    background-image:/* url('~@/assets/front-flowers-nobackground.png'),*/ linear-gradient(#8c8c8c, #4d5167); /* W3C */
+  /*  background-image: url('~@/assets/front-flowers-nobackground.png'); /* fallback */
+  background-image:
+    /* url('~@/assets/front-flowers-nobackground.png'),*/
+    linear-gradient(#8c8c8c, #4d5167);
+  /* W3C */
 
   background-repeat: no-repeat;
-  background-position:right;
+  background-position: right;
   background-attachment: fixed;
 
 }
 
-.title-projects{
+.title-projects {
   position: relative;
-  top:60%;
+  top: 60%;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
 
 
-  font-family: sans-serif,'monserrate';
+  font-family: sans-serif, 'monserrate';
   font-weight: 700;
   color: white;
-  text-shadow: 1px 1px rgb(29,27,32,0.6)
+  text-shadow: 1px 1px rgb(219, 219, 219);
 }
 
-#welcome-tour{
+#welcome-tour {
   background-color: #FAFAFA;
+  padding-top: 70px;
 }
 
-#catalog{
+#catalog {}
 
-}
-
-.catalog-header{
+.catalog-header {
   width: 100%;
   text-align: center;
   padding-bottom: 3%;
 
-  font-weight:bold;
+  font-weight: bold;
 }
-.img-circle{
+
+.img-circle {
   max-width: 100%;
   max-height: 100%;
   border-radius: 100%;
@@ -327,40 +531,105 @@ opacity: 0.5;
 }
 
 
-.image-section:hover .image-circle{
-width: 600%;
+.image-section:hover .image-circle {
+  width: 600%;
   background-color: red;
 
 
 }
-.catalog-text-header{
-  color:black;
+
+.catalog-text-header {
+  color: black;
   font-weight: bold;
 
   text-align: center;
 }
-.catalog-text{
+
+.catalog-text {
   font-weight: 700;
   font-family: 'Assistant', sans-serif;
 
   text-align: center;
 }
+
+#professional-advice {
+
+
+  background-image: url('~@/assets/professional-advice.jpg');
+
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  min-height: 600px;
+  padding: 70px 20px;
+
+
+}
+
+
+
+.professional-ad-text {
+  font-size: 1.2em;
+  font-family: 'Assistant', sans-serif;
+  font-weight: bold;
+
+  color: white;
+
+  text-shadow: 1px 1px rgb(223, 184, 65);
+
+}
+
+
+.profesional-ad-content h2 {
+  font-size: 55px;
+  line-height: 55px;
+  font-weight: 600;
+  margin: 0 0 40px;
+  color: #fff;
+  text-transform: uppercase;
+}
+
+.profesional-ad-content h3 {
+  font-size: 25px;
+  text-shadow: 0.5px 0.5px rgb(68, 68, 75);
+
+  font-weight: 700;
+  margin: 0 0 40px;
+  color: #FE3333;
+  text-transform: uppercase;
+}
+
+
+.specialities-headers {
+  text-align: center;
+  margin-bottom: 70px;
+
+}
+
+.specialities-content {
+  text-align: center;
+
+}
+
+
+
+
 @media only screen and (max-width:600px) {
-#social-menu{
+  #social-menu {
 
-  top: 90%;
-  -webkit-transform: translateY(-50%);
-  -ms-transform: translateY(-50%);
-  transform: translateY(-50%);
+    top: 90%;
+    -webkit-transform: translateY(-50%);
+    -ms-transform: translateY(-50%);
+    transform: translateY(-50%);
 
 
-  display: hidden;
-  grid-template: 1;
+    display: hidden;
+    grid-template: 1;
 
+  }
 }
-}
 
-@media (min-width: 1200px) {
-
-}
+@media (min-width: 1200px) {}
 </style>
